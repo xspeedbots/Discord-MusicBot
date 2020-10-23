77c0640033a9a52afaaf190c81b30f5b5ff6f92b
xspeedbots <73317979+xspeedbots@users.noreply.github.com>
2020-10-23 18:30:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,16 +16,7 @@ client.config = {
   prefix: process.env.PREFIX
 }
 
-//Loading Events
-fs.readdir(__dirname + "/events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    const event = require(__dirname + `/events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-    console.log("Loading Event: "+eventName)
-  });
-});
+
 
 //Loading Commands
 fs.readdir("./commands/", (err, files) => {
