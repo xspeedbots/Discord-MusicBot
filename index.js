6b3ca1240396c4b4d0d01d754809b985715cea18
xspeedbots <73317979+xspeedbots@users.noreply.github.com>
2020-10-23 18:46:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ client.queue = new Map()
 client.config = {
   prefix: process.env.PREFIX
 }
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.);
+  // Example of changing the bot's playing game to something useful. client.user is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(in ${client.guilds.cache.size} servers X-SPEED);
+});
 //Loading Commands
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
